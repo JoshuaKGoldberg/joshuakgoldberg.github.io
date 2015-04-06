dd25730d7b4cef7b19c4f44f51c2c9237b9b1bbd
Josh Goldberg <diogenesthecynic@hotmail.com>
2015-04-06 19:05:08 -0400

    Added randomization to slogans

---
+++ Scripts/index.js
@@ -0,0 +1,9 @@
+var slogans = [
+    "Passion for coding with an eye for design.",
+    "There are many like it, but this website is mine.",
+    "Clean, powerful code for projects big and small.",
+    "Code well, test well, work well.",
+    "I'm not afraid of a little DMCA notice, no sir!"
+];
+
+$("#slogan").text(slogans[Math.floor(Math.random() * slogans.length)]);
